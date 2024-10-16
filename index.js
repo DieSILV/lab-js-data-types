07ffe37b2b50621126fef3408581511230a7179f
DieSILV <59703636+DieSILV@users.noreply.github.com>
2024-10-16 15:07:03 +0200

    fix: Lab js data types solution of 5 exercises

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s1} ${s2} ${s3} ${s4}`;
+
+console.log(tongueTwister);
 
 // Print out the concatenated string
 
@@ -22,11 +25,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const formattedPart1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const formattedPart2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+const result = `${formattedPart1} ${formattedPart2}`;
 
 // Print the cameLtaiL-formatted string
-
-
+console.log(result);
 
 
 /*******************************************
@@ -35,10 +39,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +51,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +66,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+console.log(expression1); //false
 
 const expression2 = a || b;
+console.log(expression2); //true
 
 const expression3 = !a && b;
+console.log(expression3); //false
 
 const expression4 = !(a && b);
+console.log(expression4); //true
 
 const expression5 = !a || !b;
+console.log(expression5); //true
 
 const expression6 = !(a || b);
+console.log(expression6); //false
 
 const expression7 = a && a;
+console.log(expression7); //true
